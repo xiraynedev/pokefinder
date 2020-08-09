2ccb52960a0d75727b4b69c607e42941c718b1dd
xiraynedev <xiraynedev@gmail.com>
2020-08-09 18:31:08 -0400

    Updates

---
+++ precache-manifest.2a0f006c466193ac50cb376e58578e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969aa8b6dfdf43eeb9846325e5b5dab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263a72a664917ca0a2b4",
+    "url": "/static/css/main.7bc1464b.chunk.css"
+  },
+  {
+    "revision": "dc5b5b6df67f4e51d64b",
+    "url": "/static/js/2.ae44b662.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae44b662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263a72a664917ca0a2b4",
+    "url": "/static/js/main.7b7ac716.chunk.js"
+  },
+  {
+    "revision": "a370e5b6f8ce8c4f9c95",
+    "url": "/static/js/runtime-main.e4dcd0d1.js"
+  }
+]);
